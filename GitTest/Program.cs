518da5e2701e9debca374818f8113dea765f595f
123 <123@song.com>
2017-08-29 11:48:05 +0800

    新增中秋节快乐

--- GitTest/Program.cs
+++ GitTest/Program.cs
@@ -10,6 +10,8 @@ namespace GitTest
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Happy MidAutumn Festival");
+            Console.WriteLine("Happy National Day");
         }
     }
 }
